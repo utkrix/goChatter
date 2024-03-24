519820f3c6f16f76c5dbf82d0b2a50512b42b821
utkrix <utkrist991@gmail.com>
2024-03-24 13:02:40 +0545

    server init

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//"io"
+	"log"
+	"net/http"
+)
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	var uname string
+	fmt.Fprintf(w, "Hi there, Welcome to GoChatter!")
+	// io.WriteString(w, "This is my website!\n")
+	fmt.Fprintf(w, "Enter your username: ")
+	fmt.Scan(&uname)
+	fmt.Println(uname)
+}
 
-func main(){
+func main() {
+	http.HandleFunc("/chat", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
